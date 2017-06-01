1d0a40722e41149737831b837c03785a05d8d96a
Omri Gazitt <ogazitt@gmail.com>
2017-05-31 23:39:55 -0700

    added firebase sdk

---
+++ Pods/Target Support Files/Pods-TuneMinder/Pods-TuneMinder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TuneMinder : NSObject
+@end
+@implementation PodsDummy_Pods_TuneMinder
+@end
---
+++ Pods/Target Support Files/Pods-TuneMinderUITests/Pods-TuneMinderUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TuneMinderUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TuneMinderUITests
+@end
